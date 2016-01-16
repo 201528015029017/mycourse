9b8ca133e6cb1edcdd5dcd9f061ff2baa08f38ce
wusnake <wtlongsnake@163.com>
2016-01-16 05:08:06 +0000

    modify

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,9 +11,10 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require bootstrap
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
 //= require bootstrap-sprockets
-//= require bootstrap
+
